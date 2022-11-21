36db92f7d1eb7fd53a13c7d9a2c1bf7238089628
Ammar Kasem <linkamar@gmail.com>
2022-11-21 07:39:25 +0100

    many changes in term of theme and IDE

--- init.el
+++ init.el
@@ -1,15 +1,15 @@
 (setq inhibit-startup-message t)
-(scroll-bar-mode -1)			; Disable visible scrollbar
-(tool-bar-mode -1)			; Disable the toolbar
-(tooltip-mode -1)			; Disable tooltips
-(set-fringe-mode 10)			; Give some breathing room
-(global-linum-mode -1)			; Enable line numbers globally
-(menu-bar-mode -1)			; Disable the menu bar
-(global-visual-line-mode t)		; Visual line mode
+(scroll-bar-mode -1)			;; Disable visible scrollbar
+(tool-bar-mode -1)			;; Disable the toolbar
+(tooltip-mode -1)			;; Disable tooltips
+(set-fringe-mode 10)			;; Give some breathing room
+(global-linum-mode -1)			;; Enable line numbers globally
+(menu-bar-mode -1)			;; Disable the menu bar
+(global-visual-line-mode t)		;; Visual line mode
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
-(setq visible-bell t)			; Set up the visible bell
-(setq org-startup-indented t) ;To globally turn on Org Indent mode for all files
+(setq visible-bell t)			;; Set up the visible bell
+(setq org-startup-indented t) ;; To globally turn on Org Indent mode for all files
 (electric-pair-local-mode 1)
 (setq magit-view-git-manual-method 'man)
 
@@ -28,7 +28,7 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-
+(straight-use-package 'modus-themes)
 (straight-use-package 'vertico)
 (straight-use-package 'anki-editor)
 (straight-use-package 'denote)
@@ -39,7 +39,6 @@
 (straight-use-package 'todotxt-mode)
 (straight-use-package 'lsp-mode)
 (straight-use-package 'apache-mode)
-(straight-use-package 'zenburn-theme)
 (straight-use-package 'js2-mode)
 (straight-use-package 'emmet-mode)
 (straight-use-package 'web-mode)
@@ -60,9 +59,10 @@
 (straight-use-package 'counsel-projectile)
 (straight-use-package 'treemacs)
 (straight-use-package 'org-pomodoro)
+(straight-use-package 'eshell-syntax-highlighting)
+(straight-use-package 'dap-mode)
 
-(load-theme 'zenburn t) ;; theme
-(set-face-attribute 'default nil :font "DejaVu Sans Mono" :height 110)
+;;(set-face-attribute 'default nil :font "DejaVu Sans Mono" :height 110)
 (vertico-mode)
 (require 'todotxt-mode)
 
@@ -105,7 +105,7 @@
                                   :project-file "*.csproj"
 				  :compile "dotnet build"
 				  :test "dotnet test"
-				  :run "dotnet rung"
+				  :run "dotnet run"
 				  :test-suffix ".spec")
 
 (require 'web-mode)
@@ -115,3 +115,127 @@
         ("blade"  . "\\.blade\\.")
         ("cs"  . "\\.cshtml\\."))
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3199be8536de4a8300eaf9ce6d864a35aa802088c0925e944e2b74a574c68fd0" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs nil
+      modus-themes-mixed-fonts nil
+      modus-themes-subtle-line-numbers nil
+      modus-themes-intense-mouseovers nil
+      modus-themes-deuteranopia t
+      modus-themes-tabs-accented t
+      modus-themes-variable-pitch-ui nil
+      modus-themes-inhibit-reload t ; only applies to `customize-set-variable' and related
+
+      modus-themes-fringes nil ; {nil,'subtle,'intense}
+
+      ;; Options for `modus-themes-lang-checkers' are either nil (the
+      ;; default), or a list of properties that may include any of those
+      ;; symbols: `straight-underline', `text-also', `background',
+      ;; `intense' OR `faint'.
+      modus-themes-lang-checkers nil
+
+      ;; Options for `modus-themes-mode-line' are either nil, or a list
+      ;; that can combine any of `3d' OR `moody', `borderless',
+      ;; `accented', a natural number for extra padding (or a cons cell
+      ;; of padding and NATNUM), and a floating point for the height of
+      ;; the text relative to the base font size (or a cons cell of
+      ;; height and FLOAT)
+      modus-themes-mode-line '(accented borderless (padding . 4) (height . 0.9))
+
+      ;; Same as above:
+      ;; modus-themes-mode-line '(accented borderless 4 0.9)
+
+      ;; Options for `modus-themes-markup' are either nil, or a list
+      ;; that can combine any of `bold', `italic', `background',
+      ;; `intense'.
+      modus-themes-markup '(background italic)
+
+      ;; Options for `modus-themes-syntax' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
+      modus-themes-syntax `(yellow-comments)
+
+      ;; Options for `modus-themes-hl-line' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `accented', `underline', `intense'
+      modus-themes-hl-line '(underline accented)
+
+      ;; Options for `modus-themes-paren-match' are either nil (the
+      ;; default), or a list of properties that may include any of those
+      ;; symbols: `bold', `intense', `underline'
+      modus-themes-paren-match '(bold intense)
+
+      ;; Options for `modus-themes-links' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
+      ;; `bold', `italic', `background'
+      modus-themes-links '(neutral-underline background)
+
+      ;; Options for `modus-themes-box-buttons' are either nil (the
+      ;; default), or a list that can combine any of `flat', `accented',
+      ;; `faint', `variable-pitch', `underline', `all-buttons', the
+      ;; symbol of any font weight as listed in `modus-themes-weights',
+      ;; and a floating point number (e.g. 0.9) for the height of the
+      ;; button's text.
+      modus-themes-box-buttons '(variable-pitch flat faint 0.9)
+
+      ;; Options for `modus-themes-prompts' are either nil (the
+      ;; default), or a list of properties that may include any of those
+      ;; symbols: `background', `bold', `gray', `intense', `italic'
+      modus-themes-prompts '(intense bold)
+
+      ;; The `modus-themes-completions' is an alist that reads three
+      ;; keys: `matches', `selection', `popup'.  Each accepts a nil
+      ;; value (or empty list) or a list of properties that can include
+      ;; any of the following (for WEIGHT read further below):
+      ;;
+      ;; `matches' - `background', `intense', `underline', `italic', WEIGHT
+      ;; `selection' - `accented', `intense', `underline', `italic', `text-also' WEIGHT
+      ;; `popup' - same as `selected'
+      ;; `t' - applies to any key not explicitly referenced (check docs)
+      ;;
+      ;; WEIGHT is a symbol such as `semibold', `light', or anything
+      ;; covered in `modus-themes-weights'.  Bold is used in the absence
+      ;; of an explicit WEIGHT.
+      modus-themes-completions '((matches . (extrabold))
+                                 (selection . (semibold accented))
+                                 (popup . (accented intense)))
+
+      modus-themes-mail-citations nil ; {nil,'intense,'faint,'monochrome}
+
+      ;; Options for `modus-themes-region' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `no-extend', `bg-only', `accented'
+      modus-themes-region '(bg-only)
+
+      ;; Options for `modus-themes-diffs': nil, 'desaturated, 'bg-only
+      modus-themes-diffs 'desaturated
+
+      modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
+
+      modus-themes-org-agenda ; this is an alist: read the manual or its doc string
+      '((header-block . (variable-pitch 1.3))
+        (header-date . (grayscale workaholic bold-today 1.1))
+        (event . (accented varied))
+        (scheduled . uniform)
+        (habit . traffic-light))
+
+      modus-themes-headings ; this is an alist: read the manual or its doc string
+      '((1 . (overline background variable-pitch 1.3))
+        (2 . (rainbow overline 1.1))
+        (t . (semibold))))
+(load-theme 'modus-vivendi t)
