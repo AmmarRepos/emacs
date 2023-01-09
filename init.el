20d333e34c2d901a61c51012127de64604e7fbc1
Ammar Kasem <linkamar@gmail.com>
2023-01-09 07:18:21 +0100

    super key and org-pomodoro

--- init.el
+++ init.el
@@ -83,6 +83,7 @@
 (straight-use-package 'rg)
 (straight-use-package 'pulsar)
 (straight-use-package 'consult)
+(straight-use-package 'org-pomodoro)
 
 (setq ts-fold-summary-show t)
 (apheleia-global-mode +1)
@@ -339,3 +340,23 @@
 (add-hook 'imenu-after-jump-hook #'pulsar-reveal-entry)
 ;;; init.el ends here
 (put 'dired-find-alternate-file 'disabled nil)
+
+
+;; make some use of the Super key
+(define-key global-map [?\s-d] 'projectile-find-dir)
+(define-key global-map [?\s-e] 'er/expand-region)
+(define-key global-map [?\s-f] 'projectile-find-file)
+(define-key global-map [?\s-g] 'projectile-grep)
+(define-key global-map [?\s-j] 'prelude-top-join-line)
+(define-key global-map [?\s-k] 'prelude-kill-whole-line)
+(define-key global-map [?\s-l] 'goto-line)
+(define-key global-map [?\s-m] 'magit-status)
+(define-key global-map [?\s-o] 'prelude-open-line-above)
+(define-key global-map [?\s-w] 'delete-frame)
+(define-key global-map [?\s-x] 'exchange-point-and-mark)
+(define-key global-map [?\s-p] 'projectile-switch-project)
+
+(setq org-pomodoro-length 25 org-pomodoro-short-break-length 5)
+(setq org-pomodoro-mode-line 't)
+(setq org-pomodoro-keep-killed-pomodoro-time 't)
+(setq org-pomodoro-manual-break 't)
